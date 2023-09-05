6b1ac6ee6809b95b0b76fc9a03b04ebec805840f
Florent <f.gatti@agencefove.com>
2023-09-05 10:28:47 +0200

    Maj Dashboard / Wallet

--- bitchest-front/src/pages/dashboardPage/Dashboard.js
+++ bitchest-front/src/pages/dashboardPage/Dashboard.js
@@ -1,14 +1,48 @@
-import React from 'react';
+import React from "react";
+import "./Dashboard.css";
 
 function Dashboard() {
   return (
-    <div>
-      <h2>Dashboard !</h2>
-      {/* Ajoute ici le contenu de ton tableau de bord */}
+    <div className="dashboard-container bg-dark">
+      <h2>Dashboard BitChest</h2>
+
+      {/* Statistiques générales */}
+      <div className="statistics">
+        <div className="stat-item bg-items">
+          <h3>Total Investi</h3>
+          <p>$10,000</p>
+        </div>
+        <div className="stat-item bg-items">
+          <h3>Valeur actuelle</h3>
+          <p>$12,500</p>
+        </div>
+        <div className="stat-item bg-items">
+          <h3>Profit/Perte</h3>
+          <p>+ $2,500</p>
+        </div>
+      </div>
+
+      {/* Dernières transactions */}
+      <div className="transactions bg-items">
+        <h3>Dernières Transactions</h3>
+        <ul className="transaction-list">
+          <li>Achat: Bitcoin - $5000</li>
+          <li>Vente: Ethereum - $2000</li>
+          <li>Achat: Litecoin - $1500</li>
+        </ul>
+      </div>
+
+      {/* Actualités */}
+      <div className="news bg-items">
+        <h3>Dernières Actualités</h3>
+        <ul className="news-list">
+          <li>Bitcoin atteint un nouveau sommet !</li>
+          <li>Ethereum annonce des améliorations.</li>
+          <li>Le marché des cryptos pourrait voir une correction.</li>
+        </ul>
+      </div>
     </div>
   );
 }
 
 export default Dashboard;
-
-
--- bitchest-front/src/pages/walletPage/Wallet.js
+++ bitchest-front/src/pages/walletPage/Wallet.js
@@ -1,10 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Wallet.css';
 
 function Wallet() {
+  const [cryptos, setCryptos] = useState([
+    { name: 'Bitcoin', amount: 2 },
+    { name: 'Ethereum', amount: 5 },
+    { name: 'Litecoin', amount: 10 }
+  ]);
+  
+  const [euroBalance, setEuroBalance] = useState(10000); 
+  const [cryptoToSell, setCryptoToSell] = useState('Bitcoin'); 
+  const [quantityToSell, setQuantityToSell] = useState(0); 
+
+  const handleSale = () => {
+    console.log(`Vendu ${quantityToSell} ${cryptoToSell}`);
+  };
+
   return (
-    <div>
-      <h2>Gerer le Wallet !!</h2>
-      {/* Ajoute ici le contenu de ton tableau de bord */}
+    <div className="wallet-container bg-dark">
+      <h2>Gérer le Wallet</h2>
+
+      {/* Solde en euro */}
+      <div className="balance-alert bg-items">
+        Solde: {euroBalance}€
+      </div>
+
+      {/* Affichage du contenu du portefeuille */}
+      <div className="wallet-content bg-items">
+        <h3>Contenu du portefeuille :</h3>
+        <ul className="crypto-list">
+          {cryptos.map((crypto, index) => (
+            <li key={index} className="crypto-item">{crypto.name}: {crypto.amount}</li>
+          ))}
+        </ul>
+      </div>
+
+      {/* Vente d'une cryptomonnaie */}
+      <div className="sell-form bg-items">
+        <h3>Vendre une cryptomonnaie</h3>
+        
+        <div className="form-group">
+          <label>Choisissez une cryptomonnaie :</label>
+          <select className="crypto-select" value={cryptoToSell} onChange={e => setCryptoToSell(e.target.value)}>
+            {cryptos.map((crypto, index) => (
+              <option key={index} value={crypto.name}>{crypto.name}</option>
+            ))}
+          </select>
+        </div>
+
+        <div className="form-group bg-items">
+          <label>Quantité à vendre :</label>
+          <input 
+            type="number" 
+            className="crypto-input"
+            value={quantityToSell} 
+            onChange={e => setQuantityToSell(Number(e.target.value))}
+          />
+        </div>
+
+        <button className="sell-button" onClick={handleSale}>Vendre</button>
+      </div>
     </div>
   );
 }

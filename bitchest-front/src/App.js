561989459d8fba96cdcc3bfb73a186c1791c232d
Florent <f.gatti@agencefove.com>
2023-09-12 08:54:16 +0200

    Finalisation

--- bitchest-front/src/App.js
+++ bitchest-front/src/App.js
@@ -11,26 +11,12 @@ import Wallet from "./pages/walletPage/Wallet";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
 
-function handleLogin(role, setIsLoggedIn, setUserRole, fetchUserBalance) {
-  setIsLoggedIn(true);
-  setUserRole(role);
-  fetchUserBalance();
-  localStorage.setItem("isLoggedIn", "true");
-  localStorage.setItem("userRole", role);
-}
-
-function handleLogout(setIsLoggedIn, setUserRole, setUserBalance) {
-  setIsLoggedIn(false);
-  setUserRole("");
-  setUserBalance("");
-  localStorage.setItem("isLoggedIn", "false");
-  localStorage.removeItem("userRole");
-}
-
 function App() {
   const initialLoginStatus = localStorage.getItem("isLoggedIn") === "true";
-  const [userRole, setUserRole] = useState(localStorage.getItem("userRole") || "");
+  const initialRole = localStorage.getItem("userRole") || "";
+
   const [isLoggedIn, setIsLoggedIn] = useState(initialLoginStatus);
+  const [userRole, setUserRole] = useState(initialRole);
   const [userBalance, setUserBalance] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
 
@@ -50,18 +36,31 @@ function App() {
     }
   }, []);
 
+  const handleLogin = useCallback(
+    (role) => {
+      setIsLoggedIn(true);
+      setUserRole(role);
+      localStorage.setItem("isLoggedIn", "true");
+      localStorage.setItem("userRole", role);
+
+      fetchUserBalance();
+    },
+    [fetchUserBalance]
+  );
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    setUserRole("");
+    setUserBalance("");
+    localStorage.setItem("isLoggedIn", "false");
+    localStorage.removeItem("userRole");
+  };
+
   useEffect(() => {
     if (isLoggedIn) {
       fetchUserBalance();
     }
   }, [isLoggedIn, fetchUserBalance]);
-
-  const renderAdminRoute = () => {
-    if (isLoggedIn && userRole === "admin") {
-      return <ManageClients />;
-    }
-    return <Navigate to="/dashboard" replace />;
-  };
 
   return (
     <Router>
@@ -71,16 +70,16 @@ function App() {
             {errorMessage}
           </Alert>
         )}
-        {isLoggedIn && <Navbar onLogout={() => handleLogout(setIsLoggedIn, setUserRole, setUserBalance)} userRole={userRole} updateUserBalance={fetchUserBalance} userBalance={userBalance} />}
+        {isLoggedIn && <Navbar onLogout={handleLogout} userRole={userRole} updateUserBalance={fetchUserBalance} userBalance={userBalance} />}
         <div className="content">
           <Routes>
-            <Route path="/" element={!isLoggedIn ? <LoginPage onLogin={(role) => handleLogin(role, setIsLoggedIn, setUserRole, fetchUserBalance)} /> : <Navigate to="/dashboard" replace />} />
+            <Route path="/" element={!isLoggedIn ? <LoginPage onLogin={handleLogin} /> : <Navigate to="/dashboard" replace />} />
             <Route path="/dashboard" element={isLoggedIn ? <Dashboard /> : <Navigate to="/" replace />} />
             <Route path="/cryptos" element={isLoggedIn ? <CryptoConsultation userRole={userRole} /> : <Navigate to="/" replace />} />
             <Route path="/wallet" element={isLoggedIn ? <Wallet userBalance={userBalance} updateUserBalance={fetchUserBalance} /> : <Navigate to="/" replace />} />
             <Route path="/data" element={isLoggedIn ? <DataPersonel /> : <Navigate to="/" replace />} />
-            <Route path="/admin" element={renderAdminRoute()} />
-            <Route path="*" element={<Navigate to="/" replace />} /> {/* cette route doit être la dernière */}
+            <Route path="/clients" element={isLoggedIn && userRole === "admin" ? <ManageClients /> : <Navigate to="/dashboard" replace />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </div>
       </div>
